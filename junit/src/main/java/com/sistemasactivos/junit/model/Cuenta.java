badce6352a56107c8b428643a3c732103d179b67
tgriffabenitez <tgriffabenitez@gmail.com>
2023-05-19 13:25:04 -0300

    Clase cuenta

---
+++ junit/src/main/java/com/sistemasactivos/junit/model/Cuenta.java
@@ -0,0 +1,17 @@
+package com.sistemasactivos.junit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cuenta {
+    private String nombre;
+    private BigDecimal saldo;
+}
