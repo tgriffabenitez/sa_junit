c3c968b1698cc0852b63885750d84add5404a657
tgriffabenitez <tgriffabenitez@gmail.com>
2023-05-19 13:44:55 -0300

    metodo: testNombreCuenta

--- junit/src/test/java/com/sistemasactivos/junit/model/CuentaTest.java
+++ junit/src/test/java/com/sistemasactivos/junit/model/CuentaTest.java
@@ -1,17 +1,22 @@
 package com.sistemasactivos.junit.model;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CuentaTest {
 
     @Test
     void testNombreCuenta() {
-        Cuenta cuenta = new Cuenta();
-        cuenta.setNombre("Andrés");
+        Cuenta cuenta = new Cuenta("Andrés", new BigDecimal("1000.12345"));
         String esperado = "Andrés";
         String real = cuenta.getNombre();
 
-        Assertions.assertEquals(esperado, real);
+        assertEquals(esperado, real);
+        assertTrue(real.equals(esperado)); // Es lo mismo que hacer assertEquals(esperado, real);
     }
+
 }
