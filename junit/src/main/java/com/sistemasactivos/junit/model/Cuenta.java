74dc87606edfbf7a75791bfda577ba1a6371154b
tgriffabenitez <tgriffabenitez@gmail.com>
2023-05-19 14:47:54 -0300

    metodo: testReferenciaCuenta.
    Sobreescribir el metodo equals en la clase cuenta para poder usar el assertEquals en el test

--- junit/src/main/java/com/sistemasactivos/junit/model/Cuenta.java
+++ junit/src/main/java/com/sistemasactivos/junit/model/Cuenta.java
@@ -14,4 +14,23 @@ import java.math.BigDecimal;
 public class Cuenta {
     private String nombre;
     private BigDecimal saldo;
+
+    /*
+    * sobreescribo el metodo equals para que compare por atributos
+    * y no por la direccion de memoria del objeto instanciado
+    */
+    @Override
+    public boolean equals(Object obj) {
+        Cuenta cuenta = (Cuenta) obj; // casteo el objeto a tipo Cuenta
+        if (!(obj instanceof Cuenta))
+            return false;
+
+        if (this.nombre == null || this.saldo == null)
+            return false;
+
+        boolean nombreIguales = this.nombre.equals(cuenta.getNombre());
+        boolean saldoIguales = this.saldo.equals(cuenta.getSaldo());
+
+        return nombreIguales && saldoIguales;
+    }
 }
--- junit/src/test/java/com/sistemasactivos/junit/model/CuentaTest.java
+++ junit/src/test/java/com/sistemasactivos/junit/model/CuentaTest.java
@@ -26,4 +26,20 @@ class CuentaTest {
         assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
         assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
     }
+
+    @Test
+    void testReferenciaCuenta() {
+        Cuenta cuenta1 = new Cuenta("John Doe", new BigDecimal("8900.9997"));
+        Cuenta cuenta2 = new Cuenta("John Doe", new BigDecimal("8900.9997"));
+
+        /*
+        * El metodo assertEquals(cuenta1, cuenta2) compara las direcciones de memoria de los
+        * objetos intanciados.
+        *
+        * En este caso, como en la clase cuenta sobreescribi el metodo equals, ahora compara por
+        * el valor de los atributos, es decir: verifica que los nombres
+        * y el saldo sean iguales. Es por eso que este metodo ahora devuelve True
+        */
+        assertEquals(cuenta1, cuenta2);
+    }
 }
